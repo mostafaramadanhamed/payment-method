1823f664ff5d965129fe86af61fc932428793b4b
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-24 10:39:22 +0200

    - added checkout repo impl

---
+++ lib/Features/checkout/data/repos/checkout_repo_impl.dart
@@ -0,0 +1,20 @@
+import 'package:dartz/dartz.dart';
+import '../../../../core/service/error_handling.dart';
+import '../../../../core/service/stripe_service.dart';
+import '../models/payment_intent_input_model.dart';
+import 'checkout_repo.dart';
+
+class CheckoutRepoImpl implements CheckoutRepo {
+
+  final StripeService _stripeService = StripeService();
+  @override
+  Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel}) async{
+    
+    try {
+    await  _stripeService.makePayment(paymentIntentInputModel: paymentIntentInputModel);
+      return right(null);
+    } catch (e) {
+      return left(ServerFailure(message: e.toString()));
+    }
+  }
+}
