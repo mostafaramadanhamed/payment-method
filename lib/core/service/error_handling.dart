efde8cf285bd450593cae2ceefb73c360aab39f2
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-24 10:34:37 +0200

    - added checkout repo abstraction - added dartz for left and right

---
+++ lib/core/service/error_handling.dart
@@ -0,0 +1,11 @@
+
+
+abstract class Failure {
+  final String message;
+
+  Failure({required this.message});
+}
+
+class ServerFailure extends Failure {
+  ServerFailure({required super.message});
+}
