4eff84c9ac50ae51f8892e84a763d46c86a5260f
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-24 10:48:06 +0200

    - added checkout use case

---
+++ lib/Features/checkout/domain/use case/checkout_use_case.dart
@@ -0,0 +1,15 @@
+ import 'package:dartz/dartz.dart';
+
+import '../../../../core/service/error_handling.dart';
+import '../../data/models/payment_intent_input_model.dart';
+import '../../data/repos/checkout_repo.dart';
+
+class CheckoutUseCase {
+  final CheckoutRepo repository;
+
+  CheckoutUseCase(this.repository);
+
+   Future<Either<Failure, void>> makePayment({required PaymentIntentInputModel paymentIntentInputModel}) async {
+    return await repository.makePayment(paymentIntentInputModel: paymentIntentInputModel);
+  }
+ }
