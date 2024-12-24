cdb01bcf121900f3ab803712db7330fcd997495d
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-24 10:48:33 +0200

    - added checkout cubit

---
+++ lib/Features/checkout/domain/cubit/checkout_cubit.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+import '../../data/models/payment_intent_input_model.dart';
+import '../use case/checkout_use_case.dart';
+import 'checkout_state.dart';
+
+class CheckoutCubit extends Cubit<CheckoutState> {
+  final CheckoutUseCase checkoutUseCase;
+  CheckoutCubit(this.checkoutUseCase) : super(CheckoutLoading());
+
+  Future<void> makePayment({required PaymentIntentInputModel paymentIntentInputModel}) async {
+    emit(CheckoutLoading());
+    final result = await checkoutUseCase.makePayment(paymentIntentInputModel: paymentIntentInputModel);
+    result.fold(
+      (failure) => emit(CheckoutFailure(message: failure.message)),
+      (_) => emit(CheckoutSuccess(message: 'Payment Successful')),
+    );
+  }
+}
