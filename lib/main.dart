6e6f20826cbf3d41ea8ccf4b4fc769e80365bb24
mostafaramadanhamed <mostafaramadanhamed0100@gmail.com>
2024-12-25 11:08:11 +0200

    - added publishable key

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:checkout_payment_ui/Features/checkout/presentation/views/my_cart_view.dart';
+import 'package:checkout_payment_ui/core/utils/api_keys.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_stripe/flutter_stripe.dart';
 
 void main() {
+  Stripe.publishableKey = ApiKeys.publishableKey;
   runApp(const CheckoutApp());
 }
 
@@ -16,4 +19,3 @@ class CheckoutApp extends StatelessWidget {
     );
   }
 }
-
